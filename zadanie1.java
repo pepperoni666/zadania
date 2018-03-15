ae6da89dbacc3cde5a9e6b4f5fb95dfb31e9774d
pepperoni666 <peretser08@gmail.com>
2018-03-16 00:31:01 +0100

    done. Work well.

--- zadanie1.java
+++ zadanie1.java
@@ -5,7 +5,6 @@ public class zadanie1{
 	public static final void main(String[] args) throws Exception{
 		String imiona = "imiona.txt", nazwiska = "nazwiska.txt";
 		BufferedReader reader;
-		BufferedWriter fwriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("szpieg.txt")));
 
 
 		System.out.print("Podaj n: ");
@@ -20,16 +19,38 @@ public class zadanie1{
 
 		Random generator = new Random();
 		String line;
-		ArrayList<String> allNames = new ArrayList<String>(), names = new ArrayList<String>();
+		ArrayList<String> allNames = new ArrayList<String>(), names = new ArrayList<String>(), allSecNames = new ArrayList<String>(), SecNames = new ArrayList<String>(), pesel = new ArrayList<String>();
 		reader = new BufferedReader(new InputStreamReader(new FileInputStream(imiona)));
 		while((line = reader.readLine()) != null)
 			allNames.add(line);
 		reader.close();
 
+		reader = new BufferedReader(new InputStreamReader(new FileInputStream(nazwiska)));
+		while((line = reader.readLine()) != null){
+			allSecNames.add(line.split(" ")[1]);
+		}
+		reader.close();
+
 
 		for(int i = 0; i < n; i++){
-			int x = generator.nextInt(allNames.size());
+			int x = generator.nextInt(allNames.size()), y, z;
+			while(allNames.get(x).toCharArray()[allNames.get(x).length() - 1] != 'a' || allNames.get(x).equals("Jarema"))
+				x = generator.nextInt(allNames.size());
 			names.add(allNames.get(x));
+			x = generator.nextInt(allSecNames.size());
+			SecNames.add(allSecNames.get(x));
+			x = generator.nextInt(77) + 23;
+			y = generator.nextInt(12) + 1;
+			z = generator.nextInt(31) + 1;
+			pesel.add(Integer.toString(x) + ((y<10)? ("0" + y): y) + ((z<10)? ("0" + z): z) + "12345");
 		}
+
+		BufferedWriter fwriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("szpieg.txt")));
+
+		for(int i = 0; i < n; i++){
+			fwriter.write(names.get(i) + " " + SecNames.get(i) + " " + pesel.get(i));
+			fwriter.newLine();
+		}
+		fwriter.close();
 	}
 }
