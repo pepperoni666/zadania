d4c7048263ce113d84c30c05610442a3676e194e
pepperoni666 <peretser08@gmail.com>
2018-03-15 22:31:26 +0100

    first commit

---
+++ zadanie1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Tycipyci{
+	public static final void main(String[] args){
+		int n;
+		int[] abc = new int[3];
+		List<Integer[]> z = new ArrayList<Integer[]>();
+	}
+}
